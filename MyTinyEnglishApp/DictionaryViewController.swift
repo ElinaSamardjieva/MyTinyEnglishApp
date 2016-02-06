b2ca9cc2c6cdc31c2e919ef7d957a8f78bb547ca
ElinaSamardjieva <elina.sm2@gmail.com>
2016-02-06 13:12:04 +0200

    Scroll bar in text field added, keyboard in user's input hidden

--- MyTinyEnglishApp/DictionaryViewController.swift
+++ MyTinyEnglishApp/DictionaryViewController.swift
@@ -8,44 +8,34 @@
 
 import UIKit
 
-class DictionaryViewController: UIViewController {
+class DictionaryViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textInput: UITextField!
-
     @IBOutlet weak var textField: UITextView!
 
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        textInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
     @IBAction func searchWordInDictionary(sender: AnyObject) {
         let dictionary = Lexicontext.sharedDictionary()
-        //   let definition = dictionary.definitionFor("apple")
         let userWord = textInput.text
         let definition = dictionary.definitionFor(userWord)
-        //   print("The definition of \'apple' is: \n\n\(definition)\n\n")
         textField.text = definition
+        
+        //Add scroll bar
+       textField.flashScrollIndicators()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-    */
-
 }
