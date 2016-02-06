4a1fa4258ea7b0a52b637a8827e6877f8ac3d9b2
ElinaSamardjieva <elina.sm2@gmail.com>
2016-02-06 12:58:42 +0200

    Dictionary functionality added, ViewController renamed

--- MyTinyEnglishApp/DictionaryViewController.swift
+++ MyTinyEnglishApp/DictionaryViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class LibraryViewController: UIViewController {
+class DictionaryViewController: UIViewController {
 
+    @IBOutlet weak var textInput: UITextField!
+
+    @IBOutlet weak var textField: UITextView!
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +28,15 @@ class LibraryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    @IBAction func searchWordInDictionary(sender: AnyObject) {
+        let dictionary = Lexicontext.sharedDictionary()
+        //   let definition = dictionary.definitionFor("apple")
+        let userWord = textInput.text
+        let definition = dictionary.definitionFor(userWord)
+        //   print("The definition of \'apple' is: \n\n\(definition)\n\n")
+        textField.text = definition
+    }
 
     /*
     // MARK: - Navigation
